3d19b12265feeb41a6c5d7e610ec59ec0979b7fb
Евгений <THEklasterstorm@gmail.com>
2016-05-02 23:53:05 +1000

    Еще немного физики
    Записали физические свойство объектов в комменты. Может они
    когда-нибудь у них будут (типа намек)

--- Project/ProjectZXC/GameScene.swift
+++ Project/ProjectZXC/GameScene.swift
@@ -13,15 +13,48 @@ class GameScene: SKScene {
 
     }
     
+    
+    //---- SlimeBlock ----
+    //Friction 0.2
+    //Restitution 1.0
+    //Lin.Damp 0.3
+    //Ang.Damp 0.3
+    //Mass 2.0
+    
+    //---- WoodenBox ----
+    //Friction 0.2
+    //Restitution 0.2
+    //Lin.Damp 0.3
+    //Ang.Damp 0.3
+    //Mass 1.0
+    
+    //---- Main ----
+    //Friction 0.2
+    //Restitution 0.2
+    //Lin.Damp 0.2
+    //Ang.Damp 0.2
+    //Mass 1.0
+    
+    //---- Ground ----
+    //Friction 0.2
+    //Restitution 0.2
+    //Lin.Damp 0.1
+    //Ang.Damp 0.1
+    //Mass 6.0
+    
+    //---- StoneBlock ----
+    //Friction 0.3
+    //Restitution 0.2
+    //Lin.Damp 0.1
+    //Ang.Damp 0.1
+    //Mass 4.0
+    
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
         for touch in touches {
             let location = touch.locationInNode(self)
             
-            let slimeblock = SKSpriteNode(SlimeBlock)
-            slimeblock.physicsBody?.angularDamping = 1;
-            slimeblock.physicsBody?.restitution = 1;
             
             let sprite = SKSpriteNode(imageNamed:"Spaceship")
             sprite.xScale = 0.5
