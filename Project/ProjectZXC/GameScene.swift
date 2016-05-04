1b3322620439988c5fb28661a62536c89b224e8f
Евгений <THEklasterstorm@gmail.com>
2016-05-04 19:16:25 +1000

    Начал трахаться

--- Project/ProjectZXC/GameScene.swift
+++ Project/ProjectZXC/GameScene.swift
@@ -21,108 +21,60 @@ class GameScene: SKScene {
         return sprite
     }
     
+    func initGameObject(){
+        
+        let woodenBox = childNodeWithName("WoodenBox")
+        woodenBox!.xScale = 1.0
+        woodenBox!.yScale = 1.0
+        woodenBox!.physicsBody?.friction = 0.2   //Трение
+        woodenBox!.physicsBody?.restitution = 1.2    //Сила отскока
+        woodenBox!.physicsBody?.linearDamping = 1.3  //До сих пор хз что это
+        woodenBox!.physicsBody?.angularDamping = 1.3  //До сих пор хз что это
+        woodenBox!.physicsBody?.mass = 1.0   //Масса
+        woodenBox?.physicsBody?.allowsRotation = false
+
+        
+        let ground = childNodeWithName("Ground")
+        ground!.physicsBody?.friction = 0.2   //Трение
+        ground!.physicsBody?.restitution = 0.2    //Сила отскока
+        ground!.physicsBody?.linearDamping = 0.1  //До сих пор хз что это
+        ground!.physicsBody?.angularDamping = 0.1  //До сих пор хз что это
+        ground!.physicsBody?.mass = 6.0   //Масса
+      
+        let stoneBlock = childNodeWithName("StoneBlock")
+        stoneBlock!.physicsBody?.friction = 0.3   //Трение
+        stoneBlock!.physicsBody?.restitution = 0.2    //Сила отскока
+        stoneBlock!.physicsBody?.linearDamping = 0.1  //До сих пор хз что это
+        stoneBlock!.physicsBody?.angularDamping = 0.1  //До сих пор хз что это
+        stoneBlock!.physicsBody?.mass = 4.0   //Масса
+        
+        
+        //---- Main ----
+        //Friction 0.2
+        //Restitution 0.2
+        //Lin.Damp 0.2
+        //Ang.Damp 0.2
+        //Mass 1.0
+    }
     
     
     //Функция выполняемая до открытия сцены(Наверное :) )
     override func didMoveToView(view: SKView) {
+        initGameObject()
+        let slimeBlock = childNodeWithName("SlimeBlock")
+        slimeBlock?.parent?.physicsBody?.restitution = 0.2
+        slimeBlock?.parent?.xScale = 2.0
 
-        //Определяет 1 ребенка и задает ему свойства
-        //Ребенок с именем WoodenBoB (Они стоят справа)
-        let woodenBoB = childNodeWithName("WoodenBoB")
-        woodenBoB!.xScale = 2.0
-        
-
-        
-        //Просто закидываем в переменную спрайт с именем
-        //Проблема в том, что без адд смысла нет это делать
-        let woodenBox = SKSpriteNode(imageNamed: "WoodenBox")
-        woodenBox.xScale = 0.3
-        woodenBox.yScale = 0.3
-        woodenBox.physicsBody = SKPhysicsBody(rectangleOfSize: woodenBox.frame.size)    //Физ.форма прямоугольник
-        woodenBox.physicsBody?.friction = 0.1   //Трение
-        woodenBox.physicsBody?.restitution = 1.2    //Сила отскока
-        woodenBox.physicsBody?.linearDamping = 0.3  //До сих пор хз что это
-        woodenBox.physicsBody?.angularDamping = 0.3  //До сих пор хз что это
-        woodenBox.physicsBody?.mass = 1.0   //Масса
-        woodenBox.position = CGPoint(x:600,y:1700) //Позиция в поинтах
-        
-        
-        for woodenBox in children {
-            print(woodenBox,"\n")
-        }
-        
-        addChild(woodenBox) //Добавляем его на сцену
-        //insertChild(woodenBox, atIndex: 1)  // Видимо тоже добавление но с номером
-
-        
-        
-        
-        
-        
-        //Необходимо сделать, чтобы эти свойства применились для всех одинаковых объектов
     }
-
-    
     
 
-    
-    //---- SlimeBlock ----
-    //Friction 0.2
-    //Restitution 1.0
-    //Lin.Damp 0.3
-    //Ang.Damp 0.3
-    //Mass 2.0
-
-    
-    //---- WoodenBox ----
-    //Friction 0.2
-    //Restitution 0.2
-    //Lin.Damp 0.3
-    //Ang.Damp 0.3
-    //Mass 1.0
-    
-    //---- Main ----
-    //Friction 0.2
-    //Restitution 0.2
-    //Lin.Damp 0.2
-    //Ang.Damp 0.2
-    //Mass 1.0
-    
-    //---- Ground ----
-    //Friction 0.2
-    //Restitution 0.2
-    //Lin.Damp 0.1
-    //Ang.Damp 0.1
-    //Mass 6.0
-    
-    //---- StoneBlock ----
-    //Friction 0.3
-    //Restitution 0.2
-    //Lin.Damp 0.1
-    //Ang.Damp 0.1
-    //Mass 4.0
-    
-    
-    
     //Вызывается когда просиходит нажатие
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
         // Так понял цикл считывающий нажатие на экран
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            addChild(sprite)
-
-        }
+//        for touch in touches {
+//            let location = touch.locationInNode(self) //Позиция тача
+//        }
     }
    
     override func update(currentTime: CFTimeInterval) {
