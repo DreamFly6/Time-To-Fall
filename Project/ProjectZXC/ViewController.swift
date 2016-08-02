729ee965e7e5e228d6593adad9582e7645c61670
danilenkodanila <buliga96@mail.ru>
2016-08-02 17:13:55 +1000

    Говно

--- Project/ProjectZXC/ViewController.swift
+++ Project/ProjectZXC/ViewController.swift
@@ -7,7 +7,19 @@
 //
 
 import UIKit
+import GameController
+import SpriteKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func buttonTouch1(sender: AnyObject) {
+        print("нажали на кнопку button2")
+        let SecondScene = GameScene(fileNamed:"Level1")
+        let transition = SKTransition.doorsCloseHorizontalWithDuration(0.5)
+        SecondScene!.scaleMode = SKSceneScaleMode.AspectFill
+        self.scene!.view?.presentScene(SecondScene!, transition: transition)
+    }
+    
+    
 
 }
