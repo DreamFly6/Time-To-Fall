236096b5da87c1917684ace2767e356d895f5fe6
danilenkodanila <buliga96@mail.ru>
2016-11-24 13:17:10 +1000

    add normal pageViewController
    Добавил нормальный pageViewController, но есть свои траблы (куда же без
    этого) — кнопка меню всегда одного стиля. Это можно исправить,
    наверное, но чот прям жесть это. Я не представляю, как она свой стиль
    «на лету» менять будет

---
+++ Project/ProjectZXC/TutorialViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TutorialViewController.swift
+//  UIPageViewController Post
+//
+//  Created by Jeffrey Burt on 2/3/16.
+//  Copyright © 2016 Seven Even. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var containerView: UIView!
+    
+    var tutorialPageViewController: TutorialPageViewController? {
+        didSet {
+            tutorialPageViewController?.tutorialDelegate = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        pageControl.addTarget(self, action: #selector(TutorialViewController.didChangePageControlValue), for: .valueChanged)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let tutorialPageViewController = segue.destination as? TutorialPageViewController {
+            self.tutorialPageViewController = tutorialPageViewController
+        }
+    }
+
+
+}
+
+extension TutorialViewController: TutorialPageViewControllerDelegate {
+    
+    func tutorialPageViewController(_ tutorialPageViewController: TutorialPageViewController,
+        didUpdatePageCount count: Int) {
+        pageControl.numberOfPages = count
+    }
+    
+    func tutorialPageViewController(_ tutorialPageViewController: TutorialPageViewController,
+        didUpdatePageIndex index: Int) {
+        pageControl.currentPage = index
+    }
+    
+}
