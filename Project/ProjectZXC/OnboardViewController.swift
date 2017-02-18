a91265d6fc6423713166dab1193e1ad35b7d8a1e
Евгений <THEklasterstorm@gmail.com>
2017-02-18 17:19:56 +1000

    teach button upd

--- Project/ProjectZXC/OnboardViewController.swift
+++ Project/ProjectZXC/OnboardViewController.swift
@@ -21,8 +21,6 @@ class OnboardViewController: UIViewController, PaperOnboardingDataSource, PaperO
     let titleFont = UIFont(name: "AppleSDGothicNeo-UltraLight", size: 44)!
     let descFont = UIFont(name: "AppleSDGothicNeo-Thin", size: 28)!
     
-    @IBOutlet weak var btnGetStarted: UIButton!
-
     @IBOutlet weak var btnSkip: UIButton!
     @IBOutlet weak var onBoardingView: OnboardingView!
     
@@ -32,12 +30,7 @@ class OnboardViewController: UIViewController, PaperOnboardingDataSource, PaperO
         self.onBoardingView.dataSource = self
         self.onBoardingView.delegate = self
         
-        self.btnGetStarted.alpha = 0
-        self.btnGetStarted.backgroundColor = UIColor.white
-        self.btnGetStarted.setTitleColor(colorButtonBorder, for: .normal)
-        self.btnGetStarted.layer.cornerRadius = self.btnGetStarted.frame.height/2
-        self.btnGetStarted.layer.borderColor = colorButtonBorder.cgColor
-        self.btnGetStarted.layer.borderWidth = 0.5
+      
         
     }
 
@@ -56,7 +49,7 @@ class OnboardViewController: UIViewController, PaperOnboardingDataSource, PaperO
         
         return [("WoodenBox", "Wooden Box","Tap to destroy", "1", #colorLiteral(red: 0.9792104363, green: 0.7648834586, blue: 0.5689498782, alpha: 1), #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), titleFont, descFont),
                 ("StoneBlock", "Stone Block","Tap twice to destroy", "2", #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1), #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), titleFont, descFont),
-                ("ActivaBlock_On", "Волчара","Продолжение пиздатого обучения", "3", #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), titleFont, descFont)][index]
+                ("ActivaBlock_On", "Description","description", "3", #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), titleFont, descFont)][index]
     }
     
     //MARK: PaperOnboarding Delegate methods
@@ -65,38 +58,17 @@ class OnboardViewController: UIViewController, PaperOnboardingDataSource, PaperO
     }
     
     func onboardingDidTransitonToIndex(_ index: Int) {
-        if index == 2 {
-            UIView.animate(withDuration: 0.4, animations: {
-                self.btnSkip.alpha = 0
-                self.btnGetStarted.alpha = 1
-            })
-        }
     }
     
     func onboardingWillTransitonToIndex(_ index: Int) {
-        if index < 2 {
-            if self.btnGetStarted.alpha == 1 {
-                UIView.animate(withDuration: 0.4, animations: {
-                    self.btnGetStarted.alpha = 0
-                    self.btnSkip.alpha = 1
-                })
-            }
-        }
     }
     
     //MARK: IBAction
-    @IBAction func tapGetStarted(_ sender: Any) {
-        let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let MainBoard = storyboard.instantiateViewController(withIdentifier: "ViewControllerLolka")
-        self.present(MainBoard, animated: true, completion: nil)
-        
-        userDef.set(true, forKey: "OnboardStatus")
-        userDef.synchronize()
-    }
+ 
     
     @IBAction func tapSkip(_ sender: Any) {
         let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let MainBoard = storyboard.instantiateViewController(withIdentifier: "ViewControllerLolka")
+        let MainBoard = storyboard.instantiateViewController(withIdentifier: "SwipeViewController")
         self.present(MainBoard, animated: true, completion: nil)
         
         userDef.set(true, forKey: "OnboardStatus")
