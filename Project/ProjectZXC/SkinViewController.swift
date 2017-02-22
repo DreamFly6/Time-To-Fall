5fdf11e6c5ad98577b68469639896e9b71e6c09e
danilenkodanila <buliga96@mail.ru>
2017-02-23 01:12:46 +1000

    add SkinViewController
    Корявенько, но работает

---
+++ Project/ProjectZXC/SkinViewController.swift
@@ -0,0 +1,67 @@
+//
+//  SkinViewController.swift
+//  ProjectZXC
+//
+//  Created by Даниленко Данила Сергеевич on 23.02.17.
+//  Copyright © 2017 Женя. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SkinViewController: UITableViewController {
+        
+    
+    // Data model: These strings will be the data for the table view cells
+    var fruits = ["Apple", "Apricot", "Banana", "Blueberry", "Cantaloupe", "Cherry",
+                  "Clementine", "Coconut", "Cranberry", "Fig", "Grape", "Grapefruit",
+                  "Kiwi fruit", "Lemon", "Lime", "Lychee", "Mandarine", "Mango",
+                  "Melon", "Nectarine", "Olive", "Orange", "Papaya", "Peach",
+                  "Pear", "Pineapple", "Raspberry", "Strawberry"]
+    
+    // cell reuse id (cells that scroll out of view can be reused)
+    let cellReuseIdentifier = "cell"
+    
+    // don't forget to hook this up from the storyboard
+    @IBOutlet var tableSkin: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Register the table view cell class and its reuse id
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        
+        // This view controller itself will provide the delegate methods and row data for the table view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // number of rows in table view
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+    
+    // create a cell for each table view row
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        
+        let fruitName = fruits[indexPath.row]
+        cell.textLabel?.text = fruitName
+        cell.detailTextLabel?.text = "Delicious!"
+        cell.imageView?.image = UIImage(named: fruitName)
+        
+        return cell
+
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    
+    // method to run when table view cell is tapped
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
+}
