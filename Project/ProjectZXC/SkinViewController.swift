0caa2a949fe0f8f7cf1d2bcf1a06e892116a41ab
danilenkodanila <buliga96@mail.ru>
2017-02-24 01:18:27 +1000

    add transparency/fix cell
    1. Добавил прозрачность к ячейкам
    2. Добавил прозрачность с нав. бару
    3. Фиксанул ячейки — теперь только 1 активная может быть

--- Project/ProjectZXC/SkinViewController.swift
+++ Project/ProjectZXC/SkinViewController.swift
@@ -10,18 +10,35 @@ import Foundation
 import UIKit
 
 class SkinViewController: UITableViewController {
-        
+    
     
     
     // cell reuse id (cells that scroll out of view can be reused)
     let cellReuseIdentifier = "cell"
     
     // don't forget to hook this up from the storyboard
+    @IBOutlet weak var bar: UINavigationBar!
     @IBOutlet var tableSkin: UITableView!
     
+    
+    
+    
+    
     override func viewDidLoad() {
-        tableView.contentInset.top = 20
         super.viewDidLoad()
+        
+        tableView.contentInset.top = 20
+        let tempImageView = UIImageView(image: UIImage(named: "MenuBg1.png"))
+        tempImageView.frame = self.tableView.frame
+        self.tableView.backgroundView = tempImageView
+        
+        
+        
+        
+        
+        tableView.backgroundColor = .clear
+        
+        
         
         // Register the table view cell class and its reuse id
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
@@ -29,31 +46,35 @@ class SkinViewController: UITableViewController {
         // This view controller itself will provide the delegate methods and row data for the table view.
         tableView.delegate = self
         tableView.dataSource = self
+        
+        for index in 0...4 {
+            let indexPath = IndexPath(row: index, section: 0)
+            let cell = tableView.cellForRow(at: indexPath)
+            cell?.backgroundColor = .clear
+        }
+        bar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        bar.shadowImage = UIImage()
+        bar.isTranslucent = true
+        
+        
     }
     
+    
+    
     // number of rows in table view
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return fruits.count
-//    }
-//    
-//    // create a cell for each table view row
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
-//        
-//        let fruitName = fruits[indexPath.row]
-//        cell.textLabel?.text = fruitName
-//        cell.detailTextLabel?.text = "Delicious!"
-//        cell.imageView?.image = UIImage(named: fruitName)
-//        
-//        return cell
-//
-//    }
+    //    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //        return fruits.count
+    //    }
+    //
+    // create a cell for each table view row
     
     
     
     
     
-   
+    
+    
+    
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -65,6 +86,14 @@ class SkinViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("You tapped cell number \(indexPath.row).")
         let cell = tableView.cellForRow(at: indexPath)
+        
+        for index in 0...4 {
+            let indexPath = IndexPath(row: index, section: 0)
+            let cellNone = tableView.cellForRow(at: indexPath)
+            cellNone?.accessoryType = .none
+        }
+
         cell?.accessoryType = .checkmark
     }
 }
+
