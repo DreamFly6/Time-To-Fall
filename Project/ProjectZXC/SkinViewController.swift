7f096ddfa01dd3a2762ea701ece277e5bc3b9aec
danilenkodanila <buliga96@mail.ru>
2017-02-23 17:50:38 +1000

    fix tableSkin
    1. Скины статические
    2. Появился отступ от верха
    3. Добавил навигейшен бар сверху

--- Project/ProjectZXC/SkinViewController.swift
+++ Project/ProjectZXC/SkinViewController.swift
@@ -12,12 +12,6 @@ import UIKit
 class SkinViewController: UITableViewController {
         
     
-    // Data model: These strings will be the data for the table view cells
-    var fruits = ["Apple", "Apricot", "Banana", "Blueberry", "Cantaloupe", "Cherry",
-                  "Clementine", "Coconut", "Cranberry", "Fig", "Grape", "Grapefruit",
-                  "Kiwi fruit", "Lemon", "Lime", "Lychee", "Mandarine", "Mango",
-                  "Melon", "Nectarine", "Olive", "Orange", "Papaya", "Peach",
-                  "Pear", "Pineapple", "Raspberry", "Strawberry"]
     
     // cell reuse id (cells that scroll out of view can be reused)
     let cellReuseIdentifier = "cell"
@@ -26,6 +20,7 @@ class SkinViewController: UITableViewController {
     @IBOutlet var tableSkin: UITableView!
     
     override func viewDidLoad() {
+        tableView.contentInset.top = 20
         super.viewDidLoad()
         
         // Register the table view cell class and its reuse id
@@ -37,22 +32,22 @@ class SkinViewController: UITableViewController {
     }
     
     // number of rows in table view
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fruits.count
-    }
-    
-    // create a cell for each table view row
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
-        
-        let fruitName = fruits[indexPath.row]
-        cell.textLabel?.text = fruitName
-        cell.detailTextLabel?.text = "Delicious!"
-        cell.imageView?.image = UIImage(named: fruitName)
-        
-        return cell
-
-    }
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return fruits.count
+//    }
+//    
+//    // create a cell for each table view row
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+//        
+//        let fruitName = fruits[indexPath.row]
+//        cell.textLabel?.text = fruitName
+//        cell.detailTextLabel?.text = "Delicious!"
+//        cell.imageView?.image = UIImage(named: fruitName)
+//        
+//        return cell
+//
+//    }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
